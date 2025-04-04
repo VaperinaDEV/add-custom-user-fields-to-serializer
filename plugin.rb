a65858a1c467329b340a6c606eed6eca2949f793
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-04-04 22:55:00 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# name: add-country-to-serializer
-# about: Adds user_field_13 (e.g. country) to basic_user serializer for topic lists
+# name: add-custom-user-fields-to-serializer
+# about: Adds custom user fields to basic_user serializer for topic lists
 # version: 0.1
 # authors: Don
 
 after_initialize do
-  add_to_serializer(:basic_user, :user_field_13, false) do
-    object.respond_to?(:custom_fields) ? object.custom_fields["user_field_13"] : nil
+  add_to_serializer(:basic_user, :custom_fields, false) do
+    object.respond_to?(:custom_fields) ? object.custom_fields || {} : {}
   end
 end
