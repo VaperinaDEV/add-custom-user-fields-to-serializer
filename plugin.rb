325987524534364664c40cbcdf749c8d24454755
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-04-08 11:07:39 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,22 +2,39 @@
 
 # name: add-custom-user-fields-to-serializer
 # about: Adds custom user fields to basic_user serializer for topic lists and filters topics by country
-# version: 0.4
+# version: 0.5
 # authors: Don
+
+# Define a constant for the country field ID for easier configuration
+COUNTRY_FIELD_ID = "user_field_13"
 
 after_initialize do
   # Add custom_fields to basic_user serializer
-  add_to_serializer(:basic_user, :custom_fields, respect_plugin_enabled: false) do
-    object.respond_to?(:custom_fields) ? object.custom_fields || {} : {}
+  add_to_serializer(:basic_user, :custom_fields, respect_plugin_enabled: false) do 
+    object.respond_to?(:custom_fields) ? object.custom_fields || {} : {} 
+  end
+
+  # Add an index to improve performance if it doesn't exist yet
+  if !ActiveRecord::Base.connection.index_exists?(:user_custom_fields, [:name, :value])
+    begin
+      ActiveRecord::Base.connection.add_index :user_custom_fields, [:name, :value], name: 'idx_user_custom_fields_country'
+    rescue => e
+      Rails.logger.warn("Failed to create index on user_custom_fields: #{e.message}")
+    end
   end
 
   # Add custom country filter to TopicQuery
   TopicQuery.add_custom_filter(:country) do |results, topic_query|
     country = topic_query.options[:country]
     if country.present?
+      # Basic sanitization of the country parameter
+      country = country.to_s.strip
+      
       user_ids = User.joins(:user_custom_fields)
-                     .where("user_custom_fields.name = ? AND user_custom_fields.value ILIKE ?", "user_field_13", "%(#{country})%")
-                     .pluck(:id)
+        .where("user_custom_fields.name = ?", COUNTRY_FIELD_ID)
+        .where("user_custom_fields.value ILIKE ?", "%(" + country + ")%")
+        .pluck(:id)
+      
       results = results.where(user_id: user_ids)
     end
     results
@@ -40,28 +57,30 @@ after_initialize do
   end
 
   class DiscourseTopicCountryList::CountriesController < ::ApplicationController
-
     def index
-      country_field_id = "user_field_13"
+      begin
+        user_ids = Topic.select(:user_id).distinct.pluck(:user_id)
 
-      user_ids = Topic.select(:user_id).distinct.pluck(:user_id)
+        fields = UserCustomField
+          .where(user_id: user_ids, name: COUNTRY_FIELD_ID)
+          .pluck(:value)
 
-      fields = UserCustomField
-        .where(user_id: user_ids, name: country_field_id)
-        .pluck(:value)
+        countries = fields
+          .compact
+          .uniq
+          .sort
 
-      countries = fields
-        .compact
-        .uniq
-        .sort
-
-      render_json_dump(
-        countries.map do |field|
-          match = field.match(/\((.*?)\)/)
-          code = match ? match[1].downcase : "global"
-          { id: code, name: field }
-        end
-      )
+        render_json_dump(
+          countries.map do |field|
+            match = field.match(/\((.*?)\)/)
+            code = match ? match[1].downcase : "global"
+            { id: code, name: field }
+          end
+        )
+      rescue => e
+        Rails.logger.error("Error in topic-country-list API: #{e.message}")
+        render_json_error(e.message, status: 500)
+      end
     end
   end
 end
